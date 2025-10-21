09e113068718f1dbb4e4638467064159c769d6e1
devanshi gupta jpg <guptadevanshi044@gmail.com>
2025-10-22 05:28:23 +0530

    second commit

---
+++ Chapter 7 - npm and tools/app.js
@@ -0,0 +1,9 @@
+const http =require('http');
+
+const server = http.createServer((req,res) => {
+    console.log(req);
+});
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running at https://localhost:${PORT}`);
+});
